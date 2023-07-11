d0810eaeb185b1f32121b881a12c5d193fb2a5a0
mr18111 <95503534+mr18111@users.noreply.github.com>
2023-07-11 11:48:10 -0400

    Update streamlit_app.py
    Added a header and json to fruityvice connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,6 @@ import pandas
 
 import requests
 
-#bring in fruityvice:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,3 +21,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+#bring in fruityvice:
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
