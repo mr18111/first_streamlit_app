daaa51109f1b87e272af53b1e531d16ac4e5ac45
mr18111 <95503534+mr18111@users.noreply.github.com>
2023-07-11 11:53:44 -0400

    Update streamlit_app.py
    Removed the raw json and added kiwi and separated the base url from the fruit name.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 #bring in fruityvice:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 # Now we normalize the json:
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # And here we convert to dataframe:
