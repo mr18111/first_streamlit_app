10aa23007a46215f9714e6a1a68e184a26e1c810
mr18111 <95503534+mr18111@users.noreply.github.com>
2023-07-12 15:02:36 -0400

    Update streamlit_app.py
    edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
-import pandas
+#import pandas
 
-import requests
-
+#import requests
 import snowflake.connector
+
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -34,6 +35,8 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # And here we convert to dataframe:
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 #query some metadata :
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
