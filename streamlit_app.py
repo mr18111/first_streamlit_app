3bb3dd2d4b07f78bb3c137c207f2cb0819cf012e
mr18111 <95503534+mr18111@users.noreply.github.com>
2023-07-12 15:17:28 -0400

    Update streamlit_app.py
    Edit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 
-#import requests
+import requests
 import snowflake.connector
 
 from urllib.error import URLError
@@ -25,18 +25,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-"""
-streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.header("Fruityvice Fruit Advice!")
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 
 #bring in fruityvice:
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Now we normalize the json:
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # And here we convert to dataframe:
-streamlit.dataframe(fruityvice_normalized)
-"""
+#streamlit.dataframe(fruityvice_normalized)
 
 #new section to display fruityvice api response:
 streamlit.header('Fruityvice Fruit Advice!')
